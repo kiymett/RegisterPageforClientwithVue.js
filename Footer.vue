09172103fa73405c94f053c5704543ebc78d88bf
kiymett <79693713+kiymett@users.noreply.github.com>
2023-07-10 00:14:03 +0200

    Add files via upload

---
+++ Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <div class="footer_space">
+      <p>
+        &copy; Copyright 2022 SG GmbH & Co. KG. Alle Rechte vorbehalten. <br />
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+footer {
+  align-items: center;
+  text-align: center;
+  padding: 45px 0 45px;
+  position: relative;
+  display: block;
+  background-color: var(--clr-Lightgrey);
+}
+
+.footer-space {
+  margin: 10px;
+}
+</style>
